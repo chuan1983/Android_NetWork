f51fdab0c888a4701f86c772efc149aa017656fb
chuan1983 <chuan1983126@gmail.com>
2016-09-19 14:33:58 +0800

    v2

--- app/src/main/java/org/iii/tw/network/MainActivity.java
+++ app/src/main/java/org/iii/tw/network/MainActivity.java
@@ -1,13 +1,27 @@
 package org.iii.tw.network;
 
+import android.net.ConnectivityManager;
+import android.net.NetworkInfo;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.telecom.ConnectionService;
+import android.util.Log;
 
 public class MainActivity extends AppCompatActivity {
+
+    private ConnectivityManager mgr;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        mgr = (ConnectivityManager)getSystemService(CONNECTIVITY_SERVICE);
+        NetworkInfo info = mgr.getActiveNetworkInfo();
+        if(info.isConnected()){
+            Log.d("brad", "Connect ok");
+        }else{
+            Log.d("brad","Connect fail");
+        }
     }
 }
